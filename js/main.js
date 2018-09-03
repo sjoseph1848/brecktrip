87bb312656a87b0c5cb2e7782b0ad999ced4003c
Sam <sjoseph1848@gmail.com>
2018-09-03 11:03:06 -0500

    Updates

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//Responsive Nav:
+$(function() {
+  menu = $("nav ul");
+
+  $("#openup").on("click", function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+
+  $(window).resize(function() {
+    var w = $(this).width();
+    if (w > 480 && menu.is(":hidden")) {
+      menu.removeAttr("style");
+    }
+  });
+
+  $("nav li").on("click", function(e) {
+    var w = $(window).width();
+    if (w < 480) {
+      menu.slideToggle();
+    }
+  });
+  $(".open-menu").height($(window).height());
+});
+
+// Smooth Scrolling
+//first grab menu, listens for click
+$(".cf a").on("click", function(event) {
+  if (this.hash !== "") {
+    //hash is the link
+    event.preventDefault();
+
+    const hash = this.hash;
+    //animate method makes it scroll nicely
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
